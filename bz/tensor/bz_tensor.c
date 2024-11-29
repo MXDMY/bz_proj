df294e889eaaec47bbc1be7555da2c1221c0a989
MXDMY <1628109656@qq.com>
2024-11-28 22:41:45 -0500

    重构

--- bz/tensor/bz_tensor.c
+++ bz/tensor/bz_tensor.c
@@ -19,73 +19,67 @@ bz_tensor* bz_tensor_create(bz_uint* shape , bz_uint ndim , bz_dtype dtype , bz_
             count *= shape[i];
     }
 
-    // 根据数据类型，从映射表中获得每个元素的字节大小
-    bz_uint itemsize = bz_dtype_size_table[dtype];
-#if 0
-    // 根据不同的数据类型分配不同的数据内存
-    void* data = NULL;
-    if(BZ_INT_TYPE == type)
-        data = bz_mem_alloc(total * sizeof(bz_int));
-    else if(BZ_DOUBLE_TYPE == type)
-        data = bz_mem_alloc(total * sizeof(bz_dbl));
-    else
-    {
-        bz_errno = BZ_MH_DATA_TYPE_NO_SUPPORT_ERROR;
-        return NULL;
-    }
+    bz_uint itemsize = bz_dtype_size_table[dtype]; // 根据数据类型，从映射表中获得每个元素的字节大小
 
+    void* data = bz_mem_alloc(count * itemsize);
     if(NULL == data)
-    {
-        bz_errno = BZ_MEM_ALLOC_ERROR;
-        return NULL;
-    }
+        goto BZ_TENSOR_CREATE_MEM_ALLOC_ERROR;
 
-    //分配张量结构体内存
-    bz_tensor_o* obj = (bz_tensor_o*)bz_mem_alloc(sizeof(bz_tensor_o));
-    if(NULL == obj)
+    bz_uint* shape_dup = (bz_uint*)bz_mem_alloc(ndim * sizeof(bz_uint));
+    if(NULL == shape_dup)
         goto BZ_TENSOR_CREATE_FREE_DATA;
+    for(bz_uint i = 0 ; i < ndim ; i++)
+        shape_dup[i] = shape[i];
 
-    //分配张量维度数组darr内存
-    obj->darr = (bz_uint*)bz_mem_alloc(dim * sizeof(bz_uint));
-    if(NULL == obj->darr)
-        goto BZ_TENSOR_CREATE_FREE_OBJ;
-
-    //分配varr内存
-    obj->varr = (bz_uint*)bz_mem_alloc(dim * sizeof(bz_uint));
-    if(NULL == obj->varr)
-        goto BZ_TENSOR_CREATE_FREE_OBJ_DARR;
-
-    //darr赋值
-    for(bz_uint i = 0 ; i < dim ; i++)
-        obj->darr[i] = darr[i];
-    //计算varr并赋值，相比正向遍历计算O(n^2)，该方法只需要O(n)
-    obj->varr[dim - 1] = 1;
-    if(dim > 1)
+    bz_uint* offsets = (bz_uint*)bz_mem_alloc(ndim * sizeof(bz_uint));
+    if(NULL == offsets)
+        goto BZ_TENSOR_CREATE_FREE_SHAPE_DUP;
+    // 反向遍历计算 offsets，复杂度 O(n)
+    offsets[ndim - 1] = 1;
+    if(ndim > 1)
     {
-        bz_uint i = dim - 2;
-        while(1)
+        bz_uint i = ndim - 2;
+        while(true)
         {
-            obj->varr[i] = obj->varr[i + 1] * obj->darr[i + 1];
+            offsets[i] = offsets[i + 1] * shape[i + 1];
             if(0 == i)
                 break;
-            else
-                i--;
-        }    
+            i--;
+        }
     }
-    //张量其余属性赋值
-    obj->data = data;
-    obj->dim = dim;
-    obj->type = type;
-    obj->total_count = total;
-    return obj;
+    
+    bz_tensor* t = (bz_tensor*)bz_mem_alloc(sizeof(bz_tensor));
+    if(NULL == t)
+        goto BZ_TENSOR_CREATE_FREE_OFFSETS;
+    t->data = data;
+    t->shape = shape_dup;
+    t->offsets = offsets;
+    t->ndim = ndim;
+    t->count = count;
+    t->dtype = dtype;
+    t->itemsize = itemsize;
+    return t;
 
-BZ_TENSOR_CREATE_FREE_OBJ_DARR:
-    bz_mem_free(obj->darr);
-BZ_TENSOR_CREATE_FREE_OBJ:
-    bz_mem_free(obj);
+BZ_TENSOR_CREATE_FREE_OFFSETS:
+    bz_mem_free(offsets);
+BZ_TENSOR_CREATE_FREE_SHAPE_DUP:
+    bz_mem_free(shape_dup);
 BZ_TENSOR_CREATE_FREE_DATA:
     bz_mem_free(data);
-    bz_errno = BZ_MEM_ALLOC_ERROR;
-#endif
+BZ_TENSOR_CREATE_MEM_ALLOC_ERROR:
+    *errcode_p = BZ_MEM_ALLOC_ERROR;
     return NULL;
+}
+
+void bz_tensor_delete(bz_tensor** dblptr)
+{
+    if(NULL == dblptr || NULL == *dblptr)
+        return;
+    
+    bz_tensor* t = *dblptr;
+    bz_mem_free(t->data);
+    bz_mem_free(t->shape);
+    bz_mem_free(t->offsets);
+    bz_mem_free(t);
+    *dblptr = NULL;
 }
